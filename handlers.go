7c24deeacb8f8555ea4e26eede7b80c2b2bbedaa
bwrobel <bwrobel@mediaingenuity.com>
2016-12-29 12:51:47 +0000

    Todos Craete

--- handlers.go
+++ handlers.go
@@ -4,6 +4,9 @@ import (
     "encoding/json"
     "fmt"
     "net/http"
+    "io"
+    "io/ioutil"
+    "log"
 
     "github.com/gorilla/mux"
 )
@@ -15,7 +18,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
     
     w.Header().Set("Content-Type", "application/json;charset=UTF-8")
-    w.WriteHEader(http.StatusOK)
+    w.WriteHeader(http.StatusOK)
 
     if err := json.NewEncoder(w).Encode(todos); err != nil {
         panic(err)
@@ -34,15 +37,26 @@ func TodoCreate(w http.ResponseWriter, r *http.Request){
     if err != nil {
         panic(err)
     }
+    s := string(body[:])
+    log.Printf(s);
 
     if err := json.Unmarshal(body, &todo); err != nil {
+
+        if ute, ok := err.(*json.UnmarshalTypeError); ok {
+            fmt.Printf("UnmarshalTypeError %v - %v - %v\n", ute.Value, ute.Type, ute.Offset)
+        } else {
+            fmt.Println("Other error:", err)
+        }
+
         w.Header().Set("Content-Type", "application/json;charset=UTF=8")
-        w.WriteHEader(422) //unprocessable entity
+        w.WriteHeader(422) //unprocessable entity
         if err := json.NewEncoder(w).Encode(err); err != nil {
             panic(err)
         }
+        return
     }
 
+    log.Printf(todo.Name);
     t := RepoCreateTodo(todo)
     w.Header().Set("Content-Type", "application/json;charset=UTF-8")
     w.WriteHeader(http.StatusCreated)
